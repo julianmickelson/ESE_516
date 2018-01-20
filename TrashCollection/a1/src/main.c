e1d63dc50ba9c91addeed0ef3b2f0d1579191be1
McGaat <gaute_wn@hotmail.com>
2018-01-20 10:31:13 -0500

    1

--- TrashCollection/a1/src/main.c
+++ TrashCollection/a1/src/main.c
@@ -1,9 +1,9 @@
-/**
- * \file
- *
- * \brief Empty user application template
- *
- */
+#define USART_SERIAL                 USART0
+#define USART_SERIAL_ID              ID_USART0  //USART0 for sam4l
+#define USART_SERIAL_BAUDRATE        9600
+#define USART_SERIAL_CHAR_LENGTH     US_MR_CHRL_8_BIT
+#define USART_SERIAL_PARITY          US_MR_PAR_NO
+#define USART_SERIAL_STOP_BIT        US_MR_NBSTOP_1_BIT
 
 /**
  * \mainpage User Application template doxygen documentation
@@ -32,6 +32,10 @@
  * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
  */
 #include <asf.h>
+#include <stdlib.h>
+#include <conf_board.h>
+#include <usart.h>
+
 
 int main (void)
 {
@@ -40,15 +44,27 @@ int main (void)
 	system_interrupt_enable_global();
 	delay_init(); //http://asf.atmel.com/docs/3.32.0/samd21/html/group__group__common__services__delay.html
 	
+	sysclk_init();
+	board_init();
+	const sam_usart_opt_t usart_console_settings = {
+		USART_SERIAL_BAUDRATE,
+		USART_SERIAL_CHAR_LENGTH,
+		USART_SERIAL_PARITY,
+		USART_SERIAL_STOP_BIT,
+		US_MR_CHMODE_NORMAL
+	};
+	#if SAM4L
+	sysclk_enable_peripheral_clock(USART_SERIAL);
+	#else
+	sysclk_enable_peripheral_clock(USART_SERIAL_ID);
+	#endif
+	usart_init_rs232(USART_SERIAL, &usart_console_settings,
+	sysclk_get_main_hz());
+	usart_enable_tx(USART_SERIAL);
+	usart_enable_rx(USART_SERIAL);
+	
+	
 	/* This skeleton code simply sets the LED to the state of the button. */
 	while (1) {
-		/* Is button pressed? */
-		if (port_pin_get_input_level(BUTTON_0_PIN) == BUTTON_0_ACTIVE) {
-			/* Yes, so turn LED on. */
-			port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE);
-		} else {
-			/* No, so turn LED off. */
-			port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE);
-		}
 	}
 }
