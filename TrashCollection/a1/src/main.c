ff481fee2009a625ecdf04aba387de93644921f2
julianmickelson <31548233+julianmickelson@users.noreply.github.com>
2018-01-20 11:24:08 -0500

    Xplained Example

--- TrashCollection/a1/src/main.c
+++ TrashCollection/a1/src/main.c
@@ -31,40 +31,65 @@
 /*
  * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
  */
-#include <asf.h>
+/*#include <asf.h>
 #include <stdlib.h>
 #include <conf_board.h>
-#include <usart.h>
+#include <usart.h>*/
+
+struct usart_module usart_instance;
+#define MAX_RX_BUFFER_LENGTH   5
+volatile uint8_t rx_buffer[MAX_RX_BUFFER_LENGTH];
+
+void usart_read_callback(struct usart_module *const usart_module)
+{
+	usart_write_buffer_job(&usart_instance,
+	(uint8_t *)rx_buffer, MAX_RX_BUFFER_LENGTH);
+}
+void usart_write_callback(struct usart_module *const usart_module)
+{
+	port_pin_toggle_output_level(LED_0_PIN);
+}
+	
+void configure_usart(void)
+{
+	struct usart_config config_usart;
+	usart_get_config_defaults(&config_usart);
+	config_usart.baudrate    = 9600;
+	config_usart.mux_setting = EDBG_CDC_SERCOM_MUX_SETTING;
+	config_usart.pinmux_pad0 = EDBG_CDC_SERCOM_PINMUX_PAD0;
+	config_usart.pinmux_pad1 = EDBG_CDC_SERCOM_PINMUX_PAD1;
+	config_usart.pinmux_pad2 = EDBG_CDC_SERCOM_PINMUX_PAD2;
+	config_usart.pinmux_pad3 = EDBG_CDC_SERCOM_PINMUX_PAD3;
+	while (usart_init(&usart_instance,
+	EDBG_CDC_MODULE, &config_usart) != STATUS_OK) {
+	}
+	usart_enable(&usart_instance);
+}
+void configure_usart_callbacks(void)
+{
+	usart_register_callback(&usart_instance,
+	usart_write_callback, USART_CALLBACK_BUFFER_TRANSMITTED);
+	usart_register_callback(&usart_instance,
+	usart_read_callback, USART_CALLBACK_BUFFER_RECEIVED);
+	usart_enable_callback(&usart_instance, USART_CALLBACK_BUFFER_TRANSMITTED);
+	usart_enable_callback(&usart_instance, USART_CALLBACK_BUFFER_RECEIVED);
+}
 
 
 int main (void)
 {
-	//initializing
+	/*initializing
 	system_init();
 	system_interrupt_enable_global();
 	delay_init(); //http://asf.atmel.com/docs/3.32.0/samd21/html/group__group__common__services__delay.html
-	
-	sysclk_init();
-	board_init();
-	const sam_usart_opt_t usart_console_settings = {
-		USART_SERIAL_BAUDRATE,
-		USART_SERIAL_CHAR_LENGTH,
-		USART_SERIAL_PARITY,
-		USART_SERIAL_STOP_BIT,
-		US_MR_CHMODE_NORMAL
-	};
-	#if SAM4L
-	sysclk_enable_peripheral_clock(USART_SERIAL);
-	#else
-	sysclk_enable_peripheral_clock(USART_SERIAL_ID);
-	#endif
-	usart_init_rs232(USART_SERIAL, &usart_console_settings,
-	sysclk_get_main_hz());
-	usart_enable_tx(USART_SERIAL);
-	usart_enable_rx(USART_SERIAL);
-	
+	board_init();*/
+	configure_usart();
+	configure_usart_callbacks();
+	system_interrupt_enable_global();
 	
 	/* This skeleton code simply sets the LED to the state of the button. */
-	while (1) {
+	while (true) {
+		usart_read_buffer_job(&usart_instance,
+		(uint8_t *)rx_buffer, MAX_RX_BUFFER_LENGTH);
 	}
 }
